fbf64c77f444cea69de46f4082362df5682f6724
AmneetSinghh <harryamneet600@gmail.com>
2023-05-11 23:49:25 +0530

    added facade pattern and least knowledge principle

---
+++ DESIGN_PATTERN/Structural_Patterns/Facade/HeadFirstExample.java
@@ -0,0 +1,93 @@
+package DESIGN_PATTERN.Structural_Patterns.Facade;
+
+// see the example from HEAD FIRST DESIGN PATTERNS.
+public class HeadFirstExample {
+
+}
+
+// we only need to expose interface to outer world.
+
+
+// in phonepe we only expose interface to outer world/client so that they can use.
+// We don't want them to use our internal low level code to see, so we decouple them using facade interfaces.
+interface HomeThreaterI{
+    void watchmovie();
+    void endMovie();
+    void listenRadio();
+    void endRadio();
+}
+// one layer above low layer facade layer, that we expose.
+// in more complex systems we can have more then one layers of facade, to make expose less things to client
+class HomeThreaterFacade{
+
+    public void watchMovie() {
+        // lots of operations.
+//        poppper.on();
+//        popper.pop();
+//        lights.dim();
+//        screen.down();
+//        projector.on();
+//        projecter.widescreenMode();
+//        amp.on();
+//        player.on();
+//        player.playMovie();
+    }
+    public void endMovie(){
+        // lots of operations.
+        // lots of operations.
+//        poppper.off();
+//        popper.unpop();
+//        lights.undim();
+//        screen.up();
+//        projector.off();
+//        projecter.widescreenModeOff();
+//        amp.off();
+//        player.off();
+//        player.stopMovie();
+    }
+    public void listenToRadio(){
+        // lots of operations.
+    }
+    public void endRadio(){
+        // lots of operations.
+    }
+}
+
+
+
+
+// low level code. we not want the client for watching movie to use these 10 functions, so we add facade layer on top of it.
+class popper{
+
+    // lots of compositions and methods.
+}
+
+
+class amplifier{
+    // lots of compositions and methods.
+
+}
+
+class lights{
+    // lots of compositions and methods.
+
+}
+
+class screenplayer{
+    // lots of compositions and methods.
+
+}
+
+class radio{
+    // lots of compositions and methods.
+
+}
+
+
+class projector{
+    // lots of compositions and methods.
+
+}
+
+// complex set of classes  : Low layer.
+
---
+++ DESIGN_PATTERN/Structural_Patterns/Facade/PrincipleOfLeastKnowledge.java
@@ -0,0 +1,34 @@
+package DESIGN_PATTERN.Structural_Patterns.Facade;
+
+// principle of least knowledge; talk only to your immediate friends.
+public class PrincipleOfLeastKnowledge {
+}
+
+
+// DOUBLE DEPENDENCY.
+
+//public float getTemp(){
+//    return station.getThermometer().getTemperature();
+//}
+
+
+
+// WE WANT STATION TO IMPLEMENT getTemperature for us.
+// station can only talk to his compose objects and their functions or objects. not the friends of that objects.
+
+
+
+// accepted
+/*
+public float gettemp(){
+    return station.gettemperature();
+
+}
+ */
+
+
+
+
+/*
+MORE DEPENDENCY MEANS MORE COUPLE CODE, MEANS MORE CHANGES IN FUTURE IF SOMETHING CHANGE OCCURS IN DEPENDENT CLASSES.
+ */
